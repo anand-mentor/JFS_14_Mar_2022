7bc55cf8d1d6033e1fe608b7bf14754588e30064
anand-mentor <84400747+anand-mentor@users.noreply.github.com>
2022-03-17 15:11:52 +0530

    Add files via upload

--- CoreJava/shared_code/Language_fundamentals/GetterAnnotationTest.java
+++ CoreJava/shared_code/Language_fundamentals/GetterAnnotationTest.java
@@ -5,6 +5,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 @Retention(RetentionPolicy.RUNTIME)
@@ -30,17 +31,35 @@ class Sample {
 
 public class GetterAnnotationTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Class classSample = Sample.class;
 		Method methods[] = classSample.getDeclaredMethods();
 		for(Method method:methods) {
 			Annotation annotations[] = method.getAnnotations();
 			for(Annotation annotation: annotations) {
-				if(annotation.toString().equals("Getter")) {
+				if(annotation.toString().contains("Getter")) {
 					//logic to confirm whether its legal to apply Getter annotation for this method.
+					String methodName = method.getName();
+					if(methodName.startsWith("get")) {
+						String expetcedField = methodName.substring(3).toLowerCase();
+						try {
+							Field field = classSample.getDeclaredField(expetcedField);
+						}catch(NoSuchFieldException e) {
+							System.out.println("Its illegal to apply Getter annotation: " + methodName);
+						}
+					}
+					else {
+						System.out.println("Its illegal to apply Getter annotation: " + methodName);
+					}
 				}
 			}
 		}
 	}
-
 }
+
+
+
+
+
+
+
